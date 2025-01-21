443f512f9d414e44816d7f6148562af954e4b322
omiros tsintilas <h.tsintilas@gmail.com>
2025-01-21 22:27:16 +0200

    authenticate user and create and provide new token + relevant Exception

---
+++ app/Domain/Services/AuthenticationService.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Services;
+
+use App\Application\DTOs\Auth\LoginRequestDTO;
+use App\Domain\ValueObjects\Auth\AuthToken;
+use App\Exceptions\InvalidCredentialsException;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationService
+{
+    /**
+     * @throws InvalidCredentialsException
+     */
+    public function authenticateUser(LoginRequestDTO $dto): AuthToken
+    {
+        $user = User::where('email', $dto->email)->first();
+
+        if (! $user || ! Hash::check($dto->password, $user->password)) {
+            throw new InvalidCredentialsException('wrong credentials.');
+        }
+
+        $token = $user->createToken($dto->email);
+
+        return new AuthToken(
+            token: $token->plainTextToken
+        );
+    }
+
+    public function revokeCurrentToken(User $user): void
+    {
+        $user->currentAccessToken()->delete();
+    }
+}
