bdd8d3654f6066e03de1436c1a25fe79a988d771
omiros tsintilas <h.tsintilas@gmail.com>
2025-01-21 23:59:17 +0200

    use case action

---
+++ app/Application/Actions/Auth/LogoutAction.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Actions\Auth;
+
+use App\Domain\Services\AuthenticationService;
+use App\Exceptions\TokenNotFoundException;
+
+readonly class LogoutAction
+{
+    public function __construct(
+        private AuthenticationService $authService
+    )
+    {
+    }
+
+    /**
+     * @throws TokenNotFoundException
+     */
+    public function execute(): void
+    {
+        $this->authService->revokeCurrentToken();
+    }
+}
