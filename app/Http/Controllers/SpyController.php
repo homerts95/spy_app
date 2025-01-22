328e40473d5779012e094cf807535a9a5c5fa293
omiros tsintilas <h.tsintilas@gmail.com>
2025-01-22 21:28:10 +0200

    SpyController implementation currently only for create spy

--- app/Http/Controllers/SpyController.php
+++ app/Http/Controllers/SpyController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Application\Actions\CreateSpyAction;
+use App\Application\DTOs\CreateSpyDTO;
 use App\Http\Requests\CreateSpyRequest;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class SpyController extends Controller
 {
-   public function store(CreateSpyRequest $request) {
-     return $request->validated();
+    public function __construct(
+        private readonly CreateSpyAction $createSpyAction
+    ) {}
+    public function store(CreateSpyRequest $request): JsonResponse
+    {
+       $dto = CreateSpyDTO::fromRequest($request->validated());
+       $spy = $this->createSpyAction->execute($dto);
+
+        return response()->json([
+            'data' => $spy,
+            'message' => 'Spy created successfully'
+        ], Response::HTTP_CREATED);
    }
 }
